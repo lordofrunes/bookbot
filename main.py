b19fd99519f2dc7f57d4b453331cb88c0d706ab1
lordofrunes <runekenn@hotmail.com>
2025-12-02 17:30:20 +1100

    main_working

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 def main():
 
     from stats import get_num_words
+    from stats import get_characters
+    
+    
+
+    print("============ BOOKBOT ============")
+    print("Analyzing frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {get_num_words()} total words")
+    print("-------- Character Count -------")
+
+    chars = get_characters()
+    for entry in chars:
+        ch = entry["char"]
+        cnt = entry["count"]
+        if ch.isalpha():
+            print(f"{ch}: {cnt}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -10,16 +10,40 @@ def get_num_words():
     text = get_book_text("books/frankenstein.txt")
     words = text.split()
     num_words = len(words)
-    print (f"Found {num_words} total words")
+    return num_words
 
+def sort_on(items):
+    return items["count"]
 
 def get_characters():
 
+    
     text = get_book_text("books/frankenstein.txt")
+    Lower_text = text.lower()
+    characters = {}
+
+    for t in Lower_text:
+        if t in characters:
+            characters[t] += 1
+        else:
+            characters[t] = 1
+
+  #I now need to have a list of dict with key "char" for each character and a key "count" for each count
+    char_list = []
+
+    for ky in characters:
+        char_list.append({"char": ky, "count": characters[ky]})
+
+    char_list.sort(key=sort_on, reverse=True)
+
+    return char_list
+
     
 
-
+    
 
 get_num_words()
+get_characters()
+
 
 
